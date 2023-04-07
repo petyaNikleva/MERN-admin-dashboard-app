d4ad35b8ec8825ddf3971cd03612a43090e02459
P N <82761006+petyaNikleva@users.noreply.github.com>
2023-04-07 16:47:02 +0300

    add light/ dark mode and NavBar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "theme";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from 'scenes/Dashboard'
+import Layout from 'scenes/Layout';
 
 function App() {
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
   return (
     <div className="app">
-
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route element={<Layout />} >
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
